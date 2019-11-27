ed63f146394ca0330f3dc9737eacb4366c2e107d
wilschok <william.schokkele@howest.be>
2019-11-27 16:17:01 +0100

    Add console app

--- Zoo.Lib/LionCage.cs
+++ Zoo.Lib/LionCage.cs
@@ -10,12 +10,16 @@ namespace Zoo.Lib
 
         public LionCage()
         {
+
             Lion lion = new Lion("Simba", Sex.M);
             Lion lion2 = new Lion("Harambe", Sex.M);
             Lion lion3 = new Lion("Sandra", Sex.F);
+            Lions = new List<Lion>();
             Lions.Add(lion);
             Lions.Add(lion2);
             Lions.Add(lion3);
         }
+
+
     }
 }
