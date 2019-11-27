d660955b76c64aa87bb2a64c797e4d9f9c1bab33
wilschok <william.schokkele@howest.be>
2019-11-27 15:12:13 +0100

    Add project files.

---
+++ Zoo.Cons/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Zoo.Cons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Zoo.Lib/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Zoo.Lib
+{
+    public class Class1
+    {
+    }
+}
