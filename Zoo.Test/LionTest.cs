240456b538a938f35aca0bc384578e87c59aed5a
wilschok <william.schokkele@howest.be>
2019-12-11 14:36:33 +0100

    Add Xunit project and first Unit Test

---
+++ Zoo.Test/LionTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Zoo.Lib;
+
+namespace Zoo.Test
+{
+    public class LionTest
+    {
+        [Fact]
+        public void Declare_LionWithNameOnly_HasCorrectName()
+        {
+            // Arrange
+            Lion lion;
+
+            // Act
+            var name = "ABCD";
+            lion = new Lion(name);
+            var actual = lion.Name;
+
+            var expected = name;
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
