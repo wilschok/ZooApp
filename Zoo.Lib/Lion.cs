71fa0e4a043e68aed301be92a1a9a665ba16781a
wilschok <william.schokkele@howest.be>
2019-12-11 13:32:15 +0100

    Add summary for Lion

--- Zoo.Lib/Lion.cs
+++ Zoo.Lib/Lion.cs
@@ -5,6 +5,10 @@ using System.Text;
 namespace Zoo.Lib
 {
     public enum Sex { F, M };
+
+    /// <summary>
+    /// This class describes a Lion: very dangerous animal
+    /// </summary>
     public class Lion
     {  
         public Sex Sex { get; set; }
